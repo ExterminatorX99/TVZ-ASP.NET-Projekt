43dc7157ba43472457d6f76f3eec092928d9e78a
Kristijan Kos <kkos1@tvz.hr>
2021-03-16 21:54:06 +0100

    Zadatak 3.2

--- Vjezba.Model/Fakultet.cs
+++ Vjezba.Model/Fakultet.cs
@@ -29,14 +29,15 @@ namespace Vjezba.Model
 		public IEnumerable<Student> DohvatiStudente91() => Studenti.Where(student => student.DatumRodjenja().Year > 1991);
 
 		public IEnumerable<Student> DohvatiStudente91NoLinq() {
-			IList<Student> s = new List<Student>();
+			List<Student> s = new List<Student>();
 			foreach (Student student in Studenti) {
 				if (student.DatumRodjenja().Year > 1991)
 					s.Add(student);
 			}
 			return s;
 		}
-		
+
+		public IEnumerable<Student> StudentiNeTvzD() => Studenti.Where(s => s.JMBAG.Substring(0, 4) == "0246" && s.Prezime[0] == 'D');
 
 		public IEnumerable<Profesor> DohvatiProfesore() {
 			return Profesori.OrderBy(profesor => profesor.DatumIzbora);
