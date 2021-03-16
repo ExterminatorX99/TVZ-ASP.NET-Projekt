897fcb5dd83681fbfb5fd60e21206eff7266f4b4
Kristijan Kos <kkos1@tvz.hr>
2021-03-16 22:11:41 +0100

    Expression body

--- Vjezba.Model/Fakultet.cs
+++ Vjezba.Model/Fakultet.cs
@@ -14,34 +14,25 @@ namespace Vjezba.Model
 
 		public IList<Student> Studenti { get; set; }
 
-		public int KolikoProfesora() {
-			return Profesori.Count;
-		}
+		public int KolikoProfesora() => Profesori.Count;
 
-		public int KolikoStudenata() {
-			return Studenti.Count;
-		}
+		public int KolikoStudenata() => Studenti.Count;
 
-		public Student DohvatiStudenta(string jmbag) {
-			return Studenti.SingleOrDefault(s => s.JMBAG == jmbag);
-		}
+		public Student DohvatiStudenta(string jmbag) => Studenti.SingleOrDefault(s => s.JMBAG == jmbag);
 
 		public IEnumerable<Student> DohvatiStudente91() => Studenti.Where(student => student.DatumRodjenja().Year > 1991);
 		public List<Student> DohvatiStudente91List() => Studenti.Where(student => student.DatumRodjenja().Year > 1991).ToList();
 
 		public IEnumerable<Student> DohvatiStudente91NoLinq() {
-			List<Student> s = new List<Student>();
-			foreach (Student student in Studenti) {
+			var s = new List<Student>();
+			foreach (Student student in Studenti)
 				if (student.DatumRodjenja().Year > 1991)
 					s.Add(student);
-			}
 			return s;
 		}
 
 		public IEnumerable<Student> StudentiNeTvzD() => Studenti.Where(s => s.JMBAG.Substring(0, 4) == "0246" && s.Prezime[0] == 'D');
 
-		public IEnumerable<Profesor> DohvatiProfesore() {
-			return Profesori.OrderBy(profesor => profesor.DatumIzbora);
-		}
+		public IEnumerable<Profesor> DohvatiProfesore() => Profesori.OrderBy(profesor => profesor.DatumIzbora);
 	}
 }
