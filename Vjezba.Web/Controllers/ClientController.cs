61d1e1eec98b66e082c32e0a8f8874a0c4c22ff9
Kristijan Kos <kkos1@tvz.hr>
2021-04-14 21:06:07 +0200

    Zadatak 5.1

---
+++ Vjezba.Web/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Vjezba.Web.Mock;
+using Vjezba.Web.Models;
+
+namespace Vjezba.Web.Controllers
+{
+	public class ClientController : Controller
+	{
+		public IActionResult Index() {
+			List<Client> clients = MockClientRepository.Instance.All().ToList();
+
+			return View(clients);
+		}
+	}
+}
