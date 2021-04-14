950b6c0e73e14d9eabf7266dc12b2338b9eeae07
Kristijan Kos <kkos1@tvz.hr>
2021-04-14 22:56:43 +0200

    Zadatak 5.7 IQueryable

--- Vjezba.Web/Controllers/ClientController.cs
+++ Vjezba.Web/Controllers/ClientController.cs
@@ -2,30 +2,48 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Vjezba.Web.Mock;
+using Vjezba.Web.Models;
 
 namespace Vjezba.Web.Controllers
 {
 	public class ClientController : Controller
 	{
 		public IActionResult Index(string query) {
-			List<Client> clients = MockClientRepository.Instance.All().ToList();
+			IQueryable<Client> clients = MockClientRepository.Instance.All();
 
 			if (!string.IsNullOrWhiteSpace(query))
-				clients = clients.Where(c => c.FullName.ToLower().Contains(query.ToLower())).ToList();
+				clients = clients.Where(c => c.FullName.ToLower().Contains(query.ToLower()));
 
-			return View(clients);
+			return View(clients.ToList());
 		}
 
 		[HttpPost]
 		public IActionResult Index(string queryName, string queryAddress) {
-			List<Client> clients = MockClientRepository.Instance.All().ToList();
+			IQueryable<Client> clients = MockClientRepository.Instance.All();
 
 			if (!string.IsNullOrWhiteSpace(queryName))
-				clients = clients.Where(c => c.FullName.ToLower().Contains(queryName.ToLower())).ToList();
+				clients = clients.Where(c => c.FullName.ToLower().Contains(queryName.ToLower()));
 			if (!string.IsNullOrWhiteSpace(queryAddress))
-				clients = clients.Where(c => c.Address.ToLower().Contains(queryAddress.ToLower())).ToList();
+				clients = clients.Where(c => c.Address.ToLower().Contains(queryAddress.ToLower()));
 
-			return View(clients);
+			return View(clients.ToList());
+		}
+
+		[HttpPost]
+		public ActionResult AdvancedSearch(ClientFilterModel model)
+		{
+			IQueryable<Client> clients = MockClientRepository.Instance.All();
+
+			if (!string.IsNullOrWhiteSpace(model.FullName))
+				clients = clients.Where(c => c.FullName.ToLower().Contains(model.FullName.ToLower()));
+			if (!string.IsNullOrWhiteSpace(model.Email))
+				clients = clients.Where(c => c.Address.ToLower().Contains(model.Email.ToLower()));
+			if (!string.IsNullOrWhiteSpace(model.Address))
+				clients = clients.Where(c => c.Address.ToLower().Contains(model.Address.ToLower()));
+			if (!string.IsNullOrWhiteSpace(model.CityName))
+				clients = clients.Where(c => c.City != null && c.City.Name.ToLower().Contains(model.CityName.ToLower()));
+
+			return View("Index", clients.ToList());
 		}
 
 		public IActionResult Details(int? id = null) {
