e2fb201fa2a8e06b680c6de7d30f34fdf47b4f3d
Kristijan Kos <kkos1@tvz.hr>
2021-04-22 17:05:42 +0200

    Zadatak 7.5

--- Vjezba.DAL/ClientManagerDbContext.cs
+++ Vjezba.DAL/ClientManagerDbContext.cs
@@ -14,5 +14,14 @@ namespace Vjezba.DAL
 
 		protected ClientManagerDbContext() { }
 		public ClientManagerDbContext(DbContextOptions<ClientManagerDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<City>().HasData(new City { ID = 1, Name = "Zagreb" });
+			modelBuilder.Entity<City>().HasData(new City { ID = 2, Name = "New York" });
+			modelBuilder.Entity<City>().HasData(new City { ID = 3, Name = "London" });
+			modelBuilder.Entity<Client>().HasData(new Client { ID = 1, FirstName = "Kristijan", LastName = "Kos", Email = "kkos1@tvz.hr", Gender = 'M', PhoneNumber = "091-0911-091", Address = "Negdje 12", CityID = 1});
+		}
 	}
 }
---
+++ Vjezba.DAL/Migrations/20210422150407_MockData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vjezba.DAL.Migrations
+{
+    public partial class MockData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "Zagreb" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 2, "New York" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 3, "London" });
+
+            migrationBuilder.InsertData(
+                table: "Clients",
+                columns: new[] { "ID", "Address", "CityID", "Email", "FirstName", "Gender", "LastName", "PhoneNumber" },
+                values: new object[] { 1, "Negdje 12", 1, "kkos1@tvz.hr", "Kristijan", "M", "Kos", "091-0911-091" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Clients",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
