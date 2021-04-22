f96edd2a4f5829ffbce1b7c071c3c378f40745fd
Kristijan Kos <kkos1@tvz.hr>
2021-04-22 13:44:52 +0200

    Zadatak 6.4

--- Vjezba.Web/Controllers/ClientController.cs
+++ Vjezba.Web/Controllers/ClientController.cs
@@ -1,75 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Vjezba.Web.Mock;
 using Vjezba.Web.Models;
 
 namespace Vjezba.Web.Controllers
 {
-    public class ClientController : Controller
-    {
-        public IActionResult Index(string query = null)
-        {
-            var clientQuery = MockClientRepository.Instance.All();
+	public class ClientController : Controller
+	{
+		public IActionResult Index(string query = null) {
+			IQueryable<Client> clientQuery = MockClientRepository.Instance.All();
 
-            if (!string.IsNullOrWhiteSpace(query))
-                clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(query));
+			if (!string.IsNullOrWhiteSpace(query))
+				clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(query));
 
-            ViewBag.ActiveTab = 4;
+			ViewBag.ActiveTab = 4;
 
-            return View(clientQuery.ToList());
-        }
+			return View(clientQuery.ToList());
+		}
 
-        [HttpPost]
-        public ActionResult Index(string queryName, string queryAddress)
-        {
-            var clientQuery = MockClientRepository.Instance.All();
+		[HttpPost]
+		public ActionResult Index(string queryName, string queryAddress) {
+			IQueryable<Client> clientQuery = MockClientRepository.Instance.All();
 
-            //Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
-            //To rezultira optimalnijim stablom izraza koje se kvalitetnije potencijalno prevodi u SQL
-            if (!string.IsNullOrWhiteSpace(queryName))
-                clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(queryName));
+			//Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
+			//To rezultira optimalnijim stablom izraza koje se kvalitetnije potencijalno prevodi u SQL
+			if (!string.IsNullOrWhiteSpace(queryName))
+				clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(queryName));
 
-            if (!string.IsNullOrWhiteSpace(queryAddress))
-                clientQuery = clientQuery.Where(p => p.Address.ToLower().Contains(queryAddress));
+			if (!string.IsNullOrWhiteSpace(queryAddress))
+				clientQuery = clientQuery.Where(p => p.Address.ToLower().Contains(queryAddress));
 
-            ViewBag.ActiveTab = 2;
+			ViewBag.ActiveTab = 2;
 
-            var model = clientQuery.ToList();
-            return View(model);
-        }
+			List<Client> model = clientQuery.ToList();
+			return View(model);
+		}
 
-        [HttpPost]
-        public ActionResult AdvancedSearch(ClientFilterModel filter)
-        {
-            var clientQuery = MockClientRepository.Instance.All();
+		[HttpPost]
+		public ActionResult AdvancedSearch(ClientFilterModel filter) {
+			IQueryable<Client> clientQuery = MockClientRepository.Instance.All();
 
-            //Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
-            //To rezultira optimalnijim stablom izraza koje se kvalitetnije potencijalno prevodi u SQL
-            if (!string.IsNullOrWhiteSpace(filter.FullName))
-                clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(filter.FullName.ToLower()));
+			//Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
+			//To rezultira optimalnijim stablom izraza koje se kvalitetnije potencijalno prevodi u SQL
+			if (!string.IsNullOrWhiteSpace(filter.FullName))
+				clientQuery = clientQuery.Where(p => p.FullName.ToLower().Contains(filter.FullName.ToLower()));
 
-            if (!string.IsNullOrWhiteSpace(filter.Address))
-                clientQuery = clientQuery.Where(p => p.Address.ToLower().Contains(filter.Address.ToLower()));
+			if (!string.IsNullOrWhiteSpace(filter.Address))
+				clientQuery = clientQuery.Where(p => p.Address.ToLower().Contains(filter.Address.ToLower()));
 
-            if (!string.IsNullOrWhiteSpace(filter.Email))
-                clientQuery = clientQuery.Where(p => p.Email.ToLower().Contains(filter.Email.ToLower()));
+			if (!string.IsNullOrWhiteSpace(filter.Email))
+				clientQuery = clientQuery.Where(p => p.Email.ToLower().Contains(filter.Email.ToLower()));
 
-            if (!string.IsNullOrWhiteSpace(filter.City))
-                clientQuery = clientQuery.Where(p => p.City != null && p.City.Name.ToLower().Contains(filter.City.ToLower()));
+			if (!string.IsNullOrWhiteSpace(filter.City))
+				clientQuery = clientQuery.Where(p => p.City != null && p.City.Name.ToLower().Contains(filter.City.ToLower()));
 
-            ViewBag.ActiveTab = 3;
+			ViewBag.ActiveTab = 3;
 
-            var model = clientQuery.ToList();
-            return View("Index", model);
-        }
+			List<Client> model = clientQuery.ToList();
+			return View("Index", model);
+		}
 
-        public IActionResult Details(int? id = null)
-        {
-            var model = id != null ? MockClientRepository.Instance.FindByID(id.Value) : null;
-            return View(model);
-        }
-    }
+		public IActionResult Details(int? id = null) {
+			Client model = id != null ? MockClientRepository.Instance.FindByID(id.Value) : null;
+			return View(model);
+		}
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public IActionResult Create(Client client) {
+			IQueryable<Client> clients = MockClientRepository.Instance.All();
+			client.ID = clients.Max(c => c.ID) + 1;
+			client.City = clients.Select(c => c.City).FirstOrDefault(c => c != null && c.Name.Contains(client.City.Name, StringComparison.OrdinalIgnoreCase));
+
+			MockClientRepository.Instance.InsertOrUpdate(client);
+			return View();
+		}
+	}
 }
