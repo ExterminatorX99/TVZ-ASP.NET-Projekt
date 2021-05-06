e1a9d5986babf5e8fa4cbb4ae91e84be23c572fd
Kristijan Kos <kkos1@tvz.hr>
2021-05-06 15:07:45 +0200

    Why wasn't this already here?

--- Vjezba.Model/Client.cs
+++ Vjezba.Model/Client.cs
@@ -9,16 +9,21 @@ namespace Vjezba.Model
 		[Key]
 		public int ID { get; set; }
 
+		[Display(Name = "Ime")]
 		public string FirstName { get; set; }
 
+		[Display(Name = "Prezime")]
 		public string LastName { get; set; }
 
 		public string Email { get; set; }
 
+		[Display(Name = "Spol")]
 		public char Gender { get; set; }
 
+		[Display(Name = "Adresa")]
 		public string Address { get; set; }
 
+		[Display(Name = "Broj telefona")]
 		public string PhoneNumber { get; set; }
 
 		[ForeignKey(nameof(City))]
